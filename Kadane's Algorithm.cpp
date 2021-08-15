21b793ae77595b62598c4cc0306712ea0072ad72
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2021-08-16 03:52:50 +0530

    Create Kadane's Algorithm.cpp

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+    int max_global,max_current;
+    max_global=max_current=arr[0];
+    
+    for(int i=1; i<n; i++){
+        
+        max_current = max(arr[i], max_current+arr[i]);
+        
+        if(max_current>max_global){
+            max_global = max_current;
+        }
+    }
+    return max_global;
+        
+        // Your code here
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
